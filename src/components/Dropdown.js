0d51dc3aed2310cf55b526e1d45e4ba5d0786c28
Dipanjan Jayswal <iamdipanjanjayswal@gmail.com>
2021-05-28 16:07:23 +0530

    added functionality to close the dropdown whenever we click outside the dropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
   const [open, setOpen] = useState(false);
+  const ref = useRef();
+
+  useEffect(() => {
+    document.body.addEventListener("click", (event) => {
+      if (ref.current.contains(event.target)) {
+        return;
+      }
+      setOpen(false);
+    });
+  }, []);
 
   const renderedOptions = options.map((option) => {
     if (option.value === selected.value) {
@@ -18,7 +28,7 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
     );
   });
   return (
-    <div className="ui form">
+    <div ref={ref} className="ui form">
       <div className="field">
         <label className="label">Select a color</label>
         <div
